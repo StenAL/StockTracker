3a22a532e4aafdd62e25431d315238df77e011ad
Sten Laane <stenarthur.laane@real.edu.ee>
2019-02-17 13:32:51 +0000

    Stock price and currency exchange rate written to txt files

--- src/main/java/stocktracker/CurrencyConverter.java
+++ src/main/java/stocktracker/CurrencyConverter.java
@@ -1,31 +1,29 @@
 package stocktracker;
 
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import oracle.xml.parser.v2.DOMParser;
-import oracle.xml.parser.v2.XMLDocument;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import org.w3c.dom.*;
 
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import java.net.HttpURLConnection;
-import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CurrencyConverter {
 
-    XMLParser xmlParser;
+    private XMLParser xmlParser;
 
-    public static void main(String[] args) throws IOException, SAXException {
+    public static void main(String[] args)  {
         CurrencyConverter converter = new CurrencyConverter();
-        converter.test();
+        converter.writeCurrencyInfo("USD");
     }
 
     public CurrencyConverter()
@@ -33,63 +31,95 @@ public class CurrencyConverter {
         xmlParser = new XMLParser();
     }
 
-    public void test() throws IOException, SAXException {
-        System.out.println("asd");
-        String url_str = "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D.USD.EUR.SP00.A?startPeriod=2018-02-16&detail=dataonly";
-        xmlParser.downloadFile(new URL(url_str));
-        //xmlParser.parse(new URL(url_str));
+    public void writeCurrencyInfo(String currencyCode) {
+        //TODO: Add functionality to select startPeriod for currency
+        String url_str = "https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D." + currencyCode + ".EUR.SP00.A?startPeriod=2018-02-16&detail=dataonly";
+        try {
+            xmlParser.downloadXMLFile(currencyCode, new URL(url_str));
+            List<String> dataList = xmlParser.parse(currencyCode, xmlParser.dest);
+            xmlParser.writeToTextFile(currencyCode, dataList);
+            System.out.println("Fetching " + currencyCode + " done");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     private class XMLParser
     {
         DOMParser parser;
-        private void parse(URL url) throws IOException, SAXException {
-            parser = new DOMParser();
-            parser.setErrorStream(System.err);
-            parser.setValidationMode(DOMParser.DTD_VALIDATION);
-            parser.showWarnings(true);
-            //parser.parse(url);
-            XMLDocument doc = parser.getDocument();
-            System.out.print("The elements are: ");
-            printElements(doc);
 
-            System.out.println("The attributes of each element are: ");
-            //printElementAttributes(doc);
+        // destination directory!
+        String dest = "C:\\Users\\stenl\\Desktop\\jaava\\StockTracker\\src\\data";
 
-
-
-        }
-
-        private void downloadFile(URL url)  {
+        private void downloadXMLFile(String currencyCode, URL url)  {
             try {
-                String dest = "C:\\Users\\stenl\\Desktop\\jaava\\StockTracker\\data.xml";
                 final HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setDoInput(true);
                 connection.setDoOutput(true);
                 connection.setRequestMethod("GET");
                 connection.setRequestProperty("Accept", "text/xml");
 
+                //TODO: Unit testing - assert response code 200
                 System.out.println("Response code: " + connection.getResponseCode());
                 String readStream = readStream(connection.getInputStream());
                 // Give output for the command line
-                System.out.println(readStream);
+                //System.out.println(readStream);
+                List<String> lines = Arrays.asList(readStream.split("\n"));
+                Path file = Paths.get(dest + "\\" + currencyCode + "_temp_XML.xml");
+                Files.write(file, lines, Charset.forName("UTF-8"));
 
-            } catch (Exception e) {}
 
-
-                /**BufferedInputStream in = new BufferedInputStream(url.openStream());
-                FileOutputStream fileOutputStream = new FileOutputStream(dest);
-                byte dataBuffer[] = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                    fileOutputStream.write(dataBuffer, 0, bytesRead);
-                }
-                System.out.println("done");
-            }
-            catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
-            }**/
+            }
+        }
+
+
+        /**
+         * If anything ever breaks, use this:
+         * https://www.mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/
+         * @param src
+         */
+        public List<String> parse(String currencyCode, String src) {
+
+            try {
+                ArrayList<String> dataList = new ArrayList<>();
+                src += "\\" + currencyCode + "_temp_XML.xml";
+                File fXmlFile = new File(src);
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(fXmlFile);
+
+                //optional, but recommended
+                //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+                doc.getDocumentElement().normalize();
+
+                NodeList nList = doc.getElementsByTagName("Obs");
+
+                System.out.println("----------------------------");
+
+                for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                    Node nNode = nList.item(temp);
+
+                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                        Element eElement = (Element) nNode;
+                        Element date = (Element) eElement.getElementsByTagName("ObsDimension").item(0);
+                        Element exchangeRate = (Element) eElement.getElementsByTagName("ObsValue").item(0);
+
+                        //System.out.println("Date : " + date.getAttribute("value"));
+                        //System.out.println("Rate : " + exchangeRate.getAttribute("value") + "\n");
+                        String line = date.getAttribute("value") + " " + exchangeRate.getAttribute("value") + "\n";
+                        dataList.add(line);
+                    }
+                }
+                return  dataList;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
         }
 
         private String readStream(InputStream in) {
@@ -106,19 +136,17 @@ public class CurrencyConverter {
             return sb.toString();
         }
 
-        private void printElements(Document doc)
-        {
-            NodeList nl = doc.getElementsByTagName("*");
-            Node n;
-
-            for (int i=0; i<nl.getLength(); i++)
-            {
-                n = nl.item(i);
-                System.out.print(n.getNodeName() + " ");
+        private void writeToTextFile(String currencyCode, List<String> dataList) {
+            String dest = "C:\\Users\\stenl\\Desktop\\jaava\\StockTracker\\src\\data\\" + currencyCode + "_temp.txt";
+            try {
+                FileWriter writer = new FileWriter(dest);
+                for (String dataEntry: dataList) {
+                    writer.write(dataEntry);
+                    writer.flush();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            System.out.println();
         }
-
     }
 }
--- src/main/java/stocktracker/StockInfoFetcher.java
+++ src/main/java/stocktracker/StockInfoFetcher.java
@@ -1,4 +1,81 @@
 package stocktracker;
 
+import org.patriques.AlphaVantageConnector;
+import org.patriques.TimeSeries;
+import org.patriques.input.timeseries.OutputSize;
+import org.patriques.output.AlphaVantageException;
+import org.patriques.output.timeseries.Daily;
+import org.patriques.output.timeseries.data.StockData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.time.LocalDateTime;
+import java.util.*;
+
 public class StockInfoFetcher {
+
+    private static final String API_KEY = "NZ04YC2MOTE5AN4P";
+    private static final int TIMEOUT = 3000;
+
+    public static void main(String[] args) {
+        getData("IVV");
+    }
+
+    public static void getData(String ticker) {
+        Map data = fetchData(ticker);
+        writeData(data, ticker);
+
+        System.out.println("Fetcing " + ticker + " done");
+    }
+
+    public static Map fetchData(String ticker)
+    {
+        AlphaVantageConnector apiConnector = new AlphaVantageConnector(API_KEY, TIMEOUT);
+        TimeSeries stockTimeSeries = new TimeSeries(apiConnector);
+
+        try {
+            HashMap<LocalDateTime, Double> dateCloses = new HashMap<>();
+            //TODO: Let user determine outputsize using GUI
+            Daily response = stockTimeSeries.daily(ticker, OutputSize.COMPACT);
+            Map<String, String> metaData = response.getMetaData();
+            System.out.println("Information: " + metaData.get("1. Information"));
+            System.out.println("Stock: " + metaData.get("2. Symbol"));
+
+            List<StockData> stockData = response.getStockData();
+            stockData.forEach(stock -> {
+                //System.out.println("date:   " + stock.getDateTime());
+                //System.out.println("close:  " + stock.getClose());
+                dateCloses.put(stock.getDateTime(), stock.getClose());
+            });
+            return dateCloses;
+
+        } catch (AlphaVantageException e) {
+            System.out.println("something went wrong");
+        }
+        return null;
+    }
+    public static void writeData(Map data, String ticker) {
+        //System.out.println(data);
+        String filename = "C:\\Users\\stenl\\Desktop\\jaava\\StockTracker\\src\\data\\" + ticker + "_temp.txt";
+        Map<Integer, String> map = new TreeMap<Integer, String>(data);
+        //System.out.println("After Sorting:");
+        Set set2 = map.entrySet();
+        Iterator iterator2 = set2.iterator();
+        try {
+            FileWriter writer = new FileWriter(filename);
+            while (iterator2.hasNext()) {
+                Map.Entry me2 = (Map.Entry) iterator2.next();
+                //System.out.print(me2.getKey() + ": ");
+                //System.out.println(me2.getValue());
+                String writeLine = me2.getKey().toString() + " " + me2.getValue().toString() + "\n";
+                //System.out.println(writeLine);
+                writer.write(writeLine);
+                writer.flush();
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
+
